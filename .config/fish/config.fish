73d05bdc6dd633dc8d5b5acedc383256816fcce0
hhgzeng <hanhanguaizj@gmail.com>
2026-03-31 09:48:42 +0800

    重构 conda 初始化代码块，移除冗余部分并保持功能一致性

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,19 @@ fish_add_path /opt/homebrew/sbin
 set -gx EDITOR nvim
 set -gx HOMEBREW_NO_AUTO_UPDATE 1
 
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/homebrew/Caskroom/miniconda/base/bin/conda
+  eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+else
+  if test -f "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
+    . "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
+  else
+    set -x PATH "/opt/homebrew/Caskroom/miniconda/base/bin" $PATH
+  end
+end
+# <<< conda initialize <<<
+
 # ---------- 🚀 Shell 工具初始化 ----------
 eval "$(starship init fish)"
 eval "$(zoxide init fish)"
@@ -22,21 +35,6 @@ alias cd="z"
 alias ls="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
 alias cat="bat"
 alias ff="fastfetch"
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /opt/homebrew/Caskroom/miniconda/base/bin/conda
-  eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
-else
-  if test -f "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
-    . "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
-  else
-    set -x PATH "/opt/homebrew/Caskroom/miniconda/base/bin" $PATH
-  end
-end
-# <<< conda initialize <<<
-function __conda_add_prompt
-end
 
 # ---------- 其他 ----------
 # Added by OrbStack: command-line tools and integration
